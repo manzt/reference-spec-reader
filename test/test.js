63db2e0fbd51eb17c227ea2a5c580baff31a4155
Trevor Manz <trevor.j.manz@gmail.com>
2021-03-12 18:30:32 -0500

    make test dir

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 import nunjucks from "nunjucks";
 import { promises as fsp } from "fs";
-import { parse } from "./src/index.js";
+import { parse } from "../src/index.js";
 
-
-fsp.readFile('./ref.json').then(data => {
+fsp.readFile(new URL('./ref.json', import.meta.url)).then(data => {
   const spec = JSON.parse(data);
   const expanded = parse(spec, nunjucks.renderString);
   console.log(expanded);
