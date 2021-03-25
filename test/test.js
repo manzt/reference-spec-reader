b706859423af1bacf027c125d6b010fe87cce86c
Trevor Manz <trevor.j.manz@gmail.com>
2021-03-24 22:28:26 -0400

    Add tests

--- test/test.js
+++
@@ -1,9 +0,0 @@
-import nunjucks from "nunjucks";
-import { promises as fsp } from "fs";
-import { parse } from "../src/index.js";
-
-fsp.readFile(new URL('./ref.json', import.meta.url)).then(data => {
-  const spec = JSON.parse(data);
-  const expanded = parse(spec, nunjucks.renderString);
-  console.log(expanded);
-})
