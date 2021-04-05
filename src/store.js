8c73ce0199d33349034bb3966368f3a37c50b0a1
Trevor Manz <trevor.j.manz@gmail.com>
2021-04-05 17:57:12 -0400

    fix type checking

--- src/store.js
+++ src/store.js
@@ -131,9 +131,9 @@ const __toBinary = (() => {
   for (var i = 0; i < 64; i++) table[i < 26 ? i + 65 : i < 52 ? i + 71 : i < 62 ? i - 4 : i * 4 - 205] = i;
   // @ts-ignore
   return base64 => {
+    var n = base64.length;
     // @ts-ignore
-    var n = base64.length,
-      bytes = new Uint8Array((((n - (base64[n - 1] == '=') - (base64[n - 2] == '=')) * 3) / 4) | 0);
+    var bytes = new Uint8Array((((n - (base64[n - 1] == '=') - (base64[n - 2] == '=')) * 3) / 4) | 0);
     for (var i = 0, j = 0; i < n; ) {
       var c0 = table[base64.charCodeAt(i++)],
         c1 = table[base64.charCodeAt(i++)];
