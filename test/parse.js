eba3da8d1ef82e163e797212629b078acac26e62
Trevor Manz <trevor.j.manz@gmail.com>
2021-04-05 17:47:37 -0400

    Add test

--- test/parse.js
+++ test/parse.js
@@ -1,5 +1,5 @@
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
+import { test } from "uvu";
+import * as assert from "uvu/assert";
 
 import nunjucks from "nunjucks";
 import { parse } from "../src/parse.js";
@@ -20,6 +20,14 @@ const v1 = `{
           {
             "i": {"stop":  5}
           }
+      },
+      {
+        "key": "gen_key{{i}}",
+        "url": "http://{{u}}_{{i}}",
+        "dimensions":
+          {
+            "i": {"start": 5, "stop":  7}
+          }
       }   
   ],
   "refs": {
@@ -31,25 +39,29 @@ const v1 = `{
   }
 }`;
 
-const expected = new Map(Object.entries({
-  key0: 'data',
-  key1: ['http://target_url', 10000, 100],
-  key2: ['http://server.domain/path', 10000, 100],
-  key3: ['http://text', 10000, 100],
-  key4: ['http://text'],
-  gen_key0: ['http://server.domain/path_0', 1000, 1000],
-  gen_key1: ['http://server.domain/path_1', 2000, 1000],
-  gen_key2: ['http://server.domain/path_2', 3000, 1000],
-  gen_key3: ['http://server.domain/path_3', 4000, 1000],
-  gen_key4: ['http://server.domain/path_4', 5000, 1000],
-}));
+const expected = new Map(
+  Object.entries({
+    key0: "data",
+    key1: ["http://target_url", 10000, 100],
+    key2: ["http://server.domain/path", 10000, 100],
+    key3: ["http://text", 10000, 100],
+    key4: ["http://text"],
+    gen_key0: ["http://server.domain/path_0", 1000, 1000],
+    gen_key1: ["http://server.domain/path_1", 2000, 1000],
+    gen_key2: ["http://server.domain/path_2", 3000, 1000],
+    gen_key3: ["http://server.domain/path_3", 4000, 1000],
+    gen_key4: ["http://server.domain/path_4", 5000, 1000],
+    gen_key5: ["http://server.domain/path_5"],
+    gen_key6: ["http://server.domain/path_6"],
+  })
+);
 
-test('Parse references builtin', async () => {
+test("Parse references builtin", async () => {
   const spec = JSON.parse(v1);
   assert.equal(parse(spec), expected);
-})
+});
 
-test('Parse references nunjucks', async () => {
+test("Parse references nunjucks", async () => {
   const spec = JSON.parse(v1);
   assert.equal(parse(spec, nunjucks.renderString), expected);
 });
